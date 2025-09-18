17b8c0482e9b395d2419f20b7921616c8397fe98
kshitijhatwar <kshhat6685@gmail.com>
2025-09-18 23:08:23 +0530

    learned how to contact API directly

--- Kubectl_commands.sh
+++ Kubectl_commands.sh
@@ -17,4 +17,8 @@ kubectl delete pvc --all --all-namespaces
 kubectl delete pv --all
 
 --------------------------------------------------------
-#
+# Command to directl contact API server:
+#This will print nodes of cluster
+
+curl --cacert /etc/kubernetes/pki/ca.crt --cert /etc/kubernetes/pki/apiserver-kubelet-client.crt --key /etc/kubernetes/pki/apiserver-kubelet-client.key https://10.0.2.142:6443/api/v1/nodes | jq -r '.items[] | "\(.metadata.name) \(.status.conditions[] | select(.type=="Ready").status)"'
+-----------------------------------------------------------------
